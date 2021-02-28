7f656b29957fc8c1552c491e026814a106ec3687
Rahuldas D (iDEAS-Cloud Transformation) <ra40064436@wipro.com>
2021-02-28 23:43:40 +0530

    Spring Commit

---
+++ src/main/java/com/example/demo/controller/SpringBootdemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringBootdemoController {
+	
+	@RequestMapping("/")
+	public String Spring(@RequestParam(value="name",defaultValue = "World") String name) {
+		return "hello %s" + name;
+	}
+}
